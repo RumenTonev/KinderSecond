e150c2e0de54ec9805d7a71ad9d85568414e53ae
RumenTonev <rumen.tonev@yahoo.com>
2016-10-08 21:30:44 +0300

    Some quiz data caching

---
+++ KinderSecond/App_Start/CachingInitialize.cs
@@ -0,0 +1,25 @@
+﻿using KinderSecond.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Web;
+using System.Data.Entity;
+
+namespace KinderSecond.App_Start
+{
+    public class CachingInitialize
+    {
+        public static void CacheContstantData()
+        {
+            var policy = new CacheItemPolicy();
+           // policy.AbsoluteExpiration
+            using (var iow = new UowData())
+            {
+                var quizes = iow.Quizes.All().Include(x => x.Questions).Include("Questions.Answers").ToList();
+                    MemoryCache.Default.Set("quizes", quizes, policy);
+                MemoryCache.Default.Set("kur", "kuuuuuuuuu", policy);
+            }
+      }
+    }
+}
